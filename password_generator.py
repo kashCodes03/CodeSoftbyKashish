3dc6dc31259b2cd446aff6bb9eb8e8f4f07a317b
kashCodes03 <kashishparsediya0@gmail.com>
2025-08-23 18:12:37 +0530

    adding the final task to git

--- password_generator.py
+++ password_generator.py
@@ -1,4 +1,38 @@
 import string
+import random
 
-print("Lowercase:", string.ascii_lowercase)
-print("Uppercase:", string.ascii_uppercase)
+s1 = string.ascii_lowercase
+s2 = string.ascii_uppercase
+s3 = string.digits
+s4 = string.punctuation
+
+while True:
+    length = input("Enter the password length = ").lower()
+    print("for exiting the program write exit in length section")
+    if length.isdigit():
+        length=int(length)
+        s = []
+        s.extend(list(s1))
+        s.extend(list(s2))
+        s.extend(list(s3))
+        s.extend(list(s4))
+
+
+        random.shuffle(s)
+        print("your password is : ")
+        print("".join(s[0:length]))
+
+    elif length == "exit":
+        print("exiting the program!")
+        exit()
+    
+        
+    else:
+        print("invalid statement!")
+        break
+    
+
+
+
+
+
